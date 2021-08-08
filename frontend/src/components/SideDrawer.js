2ca7c95b073dd158ce1c8da0b4cb11ec09a44d0d
Ricky Memije <rickymemije@gmail.com>
2021-08-08 19:53:00 +0300

    SideDrawer.js update
    added link to SideDrawer.js and updated styles to navbar

--- frontend/src/components/SideDrawer.js
+++ frontend/src/components/SideDrawer.js
@@ -1,14 +1,29 @@
+import { Link } from "react-router-dom";
 import "./SideDrawer.css";
 
-const SideDrawer = ({show}) => {
+const SideDrawer = ({ show, click }) => {
     const sideDrawerClass = ["sidedrawer"];
 
-    if(show) {
+    if (show) {
         sideDrawerClass.push("show");
     }
 
-    return <div className={sideDrawerClass.join(" ")}></div>
-    
+    return <div className={sideDrawerClass.join(" ")}>
+        <ul className="sidedrawer__link" onClick={click}>
+            <li>
+                <Link to="/cart">
+                    <i className="fas fa-shopping-cart"></i>
+                    <span>
+                        Cart <span className="sidedrawer__cartbadge">0</span>
+                    </span>
+                </Link>
+            </li>
+            <li>
+                <Link to="/">Shop</Link>
+            </li>
+        </ul>
+    </div>
+
 };
 
 export default SideDrawer;

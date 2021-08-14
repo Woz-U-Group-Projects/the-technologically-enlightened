7375c2a6a4c1313a2942210fd8a5a937f0f1e75a
Ricky Memije <rickymemije@gmail.com>
2021-08-14 04:04:04 +0300

    Actions/Reducers completed
    Updated store.js
    added productReducers.js
    added productActions.js
    added caertActions.js

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,7 +2,14 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 
-const reducer = combineReducers ({});
+import { cartReducer } from "./reducers/cartReducers";
+import { getProductDetailsReducer, getProductsReducer } from "./reducers/productReducers";
+
+const reducer = combineReducers ({
+    cart: cartReducer,
+    getProducts: getProductsReducer,
+    getProductDetails: getProductDetailsReducer,
+});
 
 const middleware = [thunk];
 

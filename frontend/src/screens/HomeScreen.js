9a3b47ae6b2ae2582e74a09616691a6920801312
Michael Miranda <mmiranda14@stac.edu>
2021-08-15 19:58:22 -0400

    fixed responsive Home Screen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,10 +2,8 @@ import "./HomeScreen.css";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-// Components
 import Product from "../components/Product";
 
-//Actions
 import { getProducts as listProducts } from "../redux/actions/productActions";
 
 const HomeScreen = () => {

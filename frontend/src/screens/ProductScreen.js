30ebcedd93c26457d583d71a3934cca372cb2576
Michael Miranda <mmiranda14@stac.edu>
2021-08-15 22:32:39 -0400

    added more styling

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -2,7 +2,6 @@ import "./ProductScreen.css";
 import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
-// Actions
 import { getProductDetails } from "../redux/actions/productActions";
 import { addToCart } from "../redux/actions/cartActions";
 

d2cebdac8b1631bee0d021f4e44374b988986c80
Ricky Memije <rickymemije@gmail.com>
2021-08-22 04:00:52 +0300

    Updated ContactUsScreen.js
    Completed and tested form

--- frontend/src/screens/ContactUsScreen.js
+++ frontend/src/screens/ContactUsScreen.js
@@ -1,40 +1,36 @@
 import "./ContactUsScreen.css";
-//import emailjs from "emailjs-com";//
-import React from 'react'
+import emailjs from "emailjs-com";
+import React from 'react';
 
 // dependency emailjs-com is require
 
 
 
-const ContactUsScreen = () => {
+export default function ContactUsScreen() {
 
-
-    
+    function sendEmail(e) {
+      e.preventDefault();
+  
+      emailjs.sendForm('service_c4an2um', 'template_pq05pfl', e.target, 'user_5V5g6HCkkWAjG6ZFXyrDw')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+        e.target.reset()
+    }
+  
     return (
-        <div>
-            <div className="container">
-                <form>
-                    <div className="row pt-5 mx-auto">
-                        <div className="col-8 form-group mx-auto">
-                            <input type="text" className="form-control" placeholder="Name" name="name" />
-                        </div>
-                        <div className="col-8 form-group pt-2 mx-auto">
-                            <input type="email" className="form-control" placeholder="Email Address" name="email" />
-                        </div>
-                        <div className="col-8 form-group pt-2 mx-auto">
-                            <input type="text" className="form-control" placeholder="Subject" name="subject" />
-                        </div>
-                        <div className="col-8 form-group pt-2 mx-auto">
-                            <textarea className="form-control" id="" cols="38" rows="8" placeholder="Your Message" name="message" ></textarea>
-                        </div>
-                        <div className="col-8 pt-3 mx-auto">
-                            <input type="submit" className="btn btn-info" value="Send" />
-                        </div>
-                    </div>
-                </form>
-            </div>           
-        </div>
-    )
-}
-
-export default ContactUsScreen
+      <form className="contact-form" onSubmit={sendEmail}>
+        <label>Name</label>
+        <input type="text" placeholder="Name" name="name" />
+        <label>Email</label>
+        <input type="email" placeholder="Email" name="email" />
+        <label>Subject</label>
+        <input type="text" placeholder="Subject" name="subject" />
+        <label>Message</label>
+        <textarea cols="38" rows="8" placeholder="Message" name="message" />
+        <input type="submit" value="Send" />
+      </form>
+    );
+  }
